78c1e5eb1fa6b921bc53d05501a13be16992c39e
Madesh B <146665900+madeshb@users.noreply.github.com>
2024-04-07 13:17:46 +0530

    files added

---
+++ quiz.js
@@ -0,0 +1,83 @@
+const quizData = [
+    {
+        question: "What is figma?",
+        a: "A cloud based design tool",
+        b: "A social media platform",
+        c: "A project management tool",
+        d: "A programming language",
+        correct: "a",
+    },
+    {
+        question: "What is one of the key features of Figma?",
+        a: "Real-time collaboration",
+        b: "Video editing capabilities",
+        c: "Financial forecasting",
+        d: "3D modeling",
+        correct: "a",
+    },
+    {
+        question: "Which of the following best describes Figma’s design interface?",
+        a: "Intuitive and user-friendly",
+        b: "Complex and difficult to navigate",
+        c: "Text-based coding interface",
+        d: "Spreadsheet-like interface",
+        correct: "a",
+    },
+    {
+        question: "How does Figma enable collaboration among team members?",
+        a: "Real-time editing and commenting",
+        b: "Sharing design files via email",
+        c: "Sending physical copies of designs",
+        d: "Using offline design tools",
+        correct: "a",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
